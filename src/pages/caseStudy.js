9df3d7f6fa4aa491bacec8015e33510e17fb3441
dhanushp <dhanushp.work@gmail.com>
2020-04-13 21:50:57 +0530

    Rework on the Layouts and Pages

---
+++ src/pages/caseStudy.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CaseStudy = () => {
+    return (
+        <div className='page'>
+            <div className='container'>
+                <div className='row'>
+                    <h3>This is case studies page</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default caseStudy;
